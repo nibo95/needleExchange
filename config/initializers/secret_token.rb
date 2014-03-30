3b786dda77d4cbf60a19b0b46048639c1f3cc54a
Nicholas Byron <nbyron@andrew.cmu.edu>
2014-03-30 16:32:56 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NeedleExchange::Application.config.secret_key_base = '0c732760aa44fd1c25ff218a286c7b80a1ace87f09657a16e58cf71c7cf3722d911a8222f6373e20e336ba1cd1365b935030259829cd13e9759592777f302952'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NeedleExchange::Application.config.session_store :cookie_store, key: '_needleExchange_session'
