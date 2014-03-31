a23c4e4345118ae1f55ae1a6349cb2f3269bc7d7
Nicholas Byron <nbyron@andrew.cmu.edu>
2014-03-31 10:38:36 -0400

    fixed up client model

--- app/models/client.rb
+++ app/models/client.rb
@@ -19,6 +19,22 @@ class Client < ActiveRecord::Base
 	validates_presence_of :hep_c_pos
     
 	scope :new, where('new = ?', true)
+	scope :alphabetically, order('code')
+	scope :dateadded, order('register_date')
+	scope :age, order('age')
+	scope :neighborhood, lambda {|neighborhood| where("neighborhood = ?", neighborhood)}
+	scope :afamerican, where('race = ?', 'AfAm')
+	scope :white, where('race = ?', 'W')
+	scope :asian, where('race = ?', 'A')
+	scope :latin, where('race = ?', 'L')
+	scope :otherace, where('race = ?', 'O')
+	scope :males, where("gender = ?", 'M')
+	scope :females, where("gender = ?", 'F')
+	scope :transgender, where("gender = ?", 'T')
+	scope :veteran, where('veterancy = ?', true)
+	scope :hiv, where('hiv_screen = ?', true)
+	scope :hepc, where('hep_c_screen = ?', true)
+	scope :hepb, where('hep_b_screen = ?', true)
 	
 	def age
  		age = Date.today.year - birth_date.year + 1
