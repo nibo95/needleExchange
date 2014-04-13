e997ac16125a2d705629faa222071a3fc7f60d4e
Nicholas Byron <nbyron@andrew.cmu.edu>
2014-04-13 16:39:56 -0400

    made changes to locations model and view to account for migration

--- app/models/location.rb
+++ app/models/location.rb
@@ -3,6 +3,14 @@ class Location < ActiveRecord::Base
 	has_many :exchanges, through: :visits
     
 	validates_presence_of :name
+	validates_presence_of :street
+	validates_presence_of :state
+	validates_presence_of :zip
+	validates_presence_of :city
 
 	scope :alphabetical, order('name')
+
+	def address
+		return street + '\n' + city + ', ' + state + ' ' + zip
+	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140407212418) do
+ActiveRecord::Schema.define(version: 20140413202331) do
 
   create_table "clients", force: true do |t|
     t.string   "code"
@@ -62,9 +62,12 @@ ActiveRecord::Schema.define(version: 20140407212418) do
 
   create_table "locations", force: true do |t|
     t.string   "name"
-    t.string   "address"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "street"
+    t.string   "city"
+    t.string   "state"
+    t.string   "zip"
   end
 
   create_table "users", force: true do |t|
