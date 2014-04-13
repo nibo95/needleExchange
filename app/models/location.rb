98563d43f76a22da339ca454b9f6ae2c9ed48f27
Nicholas Byron <nbyron@andrew.cmu.edu>
2014-04-13 17:30:16 -0400

    made controller changes to allow for street state city and zip to be saved

--- app/models/location.rb
+++ app/models/location.rb
@@ -3,10 +3,10 @@ class Location < ActiveRecord::Base
 	has_many :exchanges, through: :visits
     
 	validates_presence_of :name
-	# validates_presence_of :street
-	# validates_presence_of :state
-	# validates_presence_of :zip
-	# validates_presence_of :city
+	validates_presence_of :street
+	validates_presence_of :state
+	validates_presence_of :zip
+	validates_presence_of :city
 
 	scope :alphabetical, -> { order('name') }
 
