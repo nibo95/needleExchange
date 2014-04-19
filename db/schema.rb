f96f9f59be6b8a514b87f94a815579775309aeba
ybjoony <byungjoy@andrew.cmu.edu>
2014-04-19 15:46:22 -0400

    did all reports

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20140413213218) do
-=======
 ActiveRecord::Schema.define(version: 20140418073705) do
->>>>>>> upstream/master
 
   create_table "clients", force: true do |t|
     t.string   "code"
